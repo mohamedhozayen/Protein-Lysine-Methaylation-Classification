879c50d01dca8202a20ffd0c8299d5c03be338ad
mohamedhozayen <mohamedhozayena@gmail.com>
2019-11-30 18:39:37 -0500

    update

--- pca-kernal.py
+++ pca-kernal.py
@@ -47,6 +47,7 @@ X_pca = pca.fit_transform(X)
 plt.figure()
 #plt.subplot(2, 2, 1, aspect='equal')
 plt.title("Original space")
+plt.figure(figsize=(25,15))
 reds = y == 0
 blues = y == 1
 
@@ -83,9 +84,56 @@ blues = y == 1
 
 plt.subplot(2, 2, 4, aspect='equal')
 plt.scatter(X_back[reds, 0], X_back[reds, 1], c="red",
-            s=20, edgecolor='k')
+            s=20, edgecolor='k', alpha=0.5)
+plt.title("Original space after inverse transform")
+plt.xlabel("$x_1$")
+plt.ylabel("$x_2$")
+
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+#plt.subplot(2, 2, 1, aspect='equal')
+plt.title("Original space")
+plt.figure(figsize=(25,15))
+reds = y == 0
+blues = y == 1
+
+#plt.scatter(X[reds, 0], X[reds, 1], c="red",
+#            s=20, edgecolor='k')
+#plt.scatter(X[blues, 0], X[blues, 1], c="blue",
+#            s=20, edgecolor='k')
+#plt.xlabel("$x_1$")
+#plt.ylabel("$x_2$")
+#
+#X1, X2 = np.meshgrid(np.linspace(-1.5, 1.5, 50), np.linspace(-1.5, 1.5, 50))
+#X_grid = np.array([np.ravel(X1), np.ravel(X2)]).T
+## projection on the first principal component (in the phi space)
+#Z_grid = kpca.transform(X_grid)[:, 0].reshape(X1.shape)
+#plt.contour(X1, X2, Z_grid, colors='grey', linewidths=1, origin='lower')
+#
+#plt.subplot(2, 2, 2, aspect='equal')
+#plt.scatter(X_pca[reds, 0], X_pca[reds, 1], c="red",
+#            s=20, edgecolor='k')
+#plt.scatter(X_pca[blues, 0], X_pca[blues, 1], c="blue",
+#            s=20, edgecolor='k')
+#plt.title("Projection by PCA")
+#plt.xlabel("1st principal component")
+#plt.ylabel("2nd component")
+#
+#plt.subplot(2, 2, 3, aspect='equal')
+#plt.scatter(X_kpca[reds, 0], X_kpca[reds, 1], c="red",
+#            s=20, edgecolor='k')
+#plt.scatter(X_kpca[blues, 0], X_kpca[blues, 1], c="blue",
+#            s=20, edgecolor='k')
+#plt.title("Projection by KPCA")
+#plt.xlabel(r"1st principal component in space induced by $\phi$")
+#plt.ylabel("2nd component")
+
+plt.subplot(2, 2, 4, aspect='equal')
+
 plt.scatter(X_back[blues, 0], X_back[blues, 1], c="blue",
-            s=20, edgecolor='k')
+            s=20, edgecolor='k', alpha=0.5)
 plt.title("Original space after inverse transform")
 plt.xlabel("$x_1$")
 plt.ylabel("$x_2$")
