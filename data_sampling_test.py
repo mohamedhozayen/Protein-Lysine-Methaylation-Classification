63e93606ccde65a7ed7ae571d03ee8414223b13c
Ben Earle <ben.earle@live.ca>
2019-11-30 16:18:25 -0800

    Scaling

--- data_sampling_test.py
+++ data_sampling_test.py
@@ -23,13 +23,39 @@ from sklearn.metrics import precision_recall_curve
 from imblearn.over_sampling import SMOTE
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.under_sampling import *
+from sklearn import preprocessing
+import preprocessing as prc
+import feature_selection as fs
+
+
+def report(y_true, y_pred, y_prob, name):
+	print(name)
+	cm = confusion_matrix(y_true, y_pred)
+	print("TN = " + str(cm[0][0]))
+	print("FP = " + str(cm[0][1]))
+	print("FN = " + str(cm[1][0]))
+	print("TP = " + str(cm[1][1]))
+	print("Pr = " + str(cm[1][1]/(cm[1][1] + cm[0][1])))
+	print("Re = " + str(cm[1][1]/(cm[1][1] + cm[1][0])))
+	print("Confusion Matrix: \n" + str(cm))
+	print()
+
+	precision, recall, thresholds = precision_recall_curve( y_true, y_prob)
+	average_precision = average_precision_score(y_true, y_prob)
+	plt.plot(recall, precision,label=name+", auc = {0:.4f}".format(average_precision))
+treeDepth = 2
 # generate 2 class dataset
 # Import data
-df = pd.read_csv("csv_result-Descriptors_Training.csv")
+df = pd.read_csv('csv_result-Descriptors_Training.csv', sep=',') 
+df = df.drop(['id'], axis=1).replace(['P', 'N'], [1, 0])
+# df = prc.handle_outlier(prc.detect_outlier_iterative_IQR(df).dropna(thresh=20))
 
 # Split into train and test
-X = df.drop(['id', 'class'], axis=1)
+X = df.drop(['class'], axis=1)
 y = df['class']
+X_std = pd.DataFrame(preprocessing.normalize(X), columns=X.columns)
+scaler = preprocessing.StandardScaler()
+X_scl = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
 
 # NOTE Stratified KFold!
 kf = StratifiedKFold(n_splits=5, shuffle = True)
@@ -39,6 +65,14 @@ y_true = []
 reg_dt = True
 dt_y_pred = []
 dt_y_prob = []
+
+reg_dt_std = True
+dt_std_y_pred = []
+dt_std_y_prob = []
+
+reg_dt_scl =True
+dt_scl_y_pred = []
+dt_scl_y_prob = []
 
 rand = True
 dc_y_pred = []
@@ -56,15 +90,28 @@ smote_dt = True
 dts_y_pred = []
 dts_y_prob = []
 
-clst_dt = True
+over_dt_scl = True
+dto_scl_y_pred = []
+dto_scl_y_prob = []
+
+under_dt_scl = True
+dtu_scl_y_pred = []
+dtu_scl_y_prob = []
+
+smote_dt_scl = True
+dts_scl_y_pred = []
+dts_scl_y_prob = []
+
+clst_dt = False
 dtc_y_pred = []
 dtc_y_prob = []
 
 for train_index, test_index in kf.split(X, y):
 	# Train and test modelpip ion
 	X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+	X_train_std, X_test_std = X_std.iloc[train_index], X_std.iloc[test_index]
+	X_train_scl, X_test_scl = X_scl.iloc[train_index], X_scl.iloc[test_index]
 	y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-	# y_true.extend(y_test == 'P')
 	y_true.extend(y_test)
 
 	#Dummy classifier
@@ -76,10 +123,51 @@ for train_index, test_index in kf.split(X, y):
 
 	# Clasic decision tree
 	if reg_dt:
-		dt = DecisionTreeClassifier()
+		dt = DecisionTreeClassifier(max_depth = treeDepth)
 		dt.fit(X_train, y_train)
 		dt_y_pred.extend(dt.predict(X_test).tolist())
 		dt_y_prob.extend(dt.predict_proba(X_test)[::,1])
+
+	# Clasic decision tree standardized data
+	if reg_dt_std:
+		dt_std = DecisionTreeClassifier(max_depth = treeDepth)
+		dt_std.fit(X_train_std, y_train)
+		dt_std_y_pred.extend(dt_std.predict(X_test_std).tolist())
+		dt_std_y_prob.extend(dt_std.predict_proba(X_test_std)[::,1])
+	
+	# Clasic decision tree scaled data
+	if reg_dt_scl:
+		dt_scl = DecisionTreeClassifier(max_depth = treeDepth)
+		dt_scl.fit(X_train_scl, y_train)
+		dt_scl_y_pred.extend(dt_scl.predict(X_test_scl).tolist())
+		dt_scl_y_prob.extend(dt_scl.predict_proba(X_test_scl)[::,1])
+
+	# DT trained on oversampled data set
+	if over_dt_scl:
+		ros = RandomOverSampler()
+		X_ros, y_ros = ros.fit_sample(X_train_scl, y_train)
+		dto_scl = DecisionTreeClassifier()
+		dto_scl.fit(X_ros, y_ros)
+		dto_scl_y_pred.extend(dto_scl.predict(X_test_scl).tolist())
+		dto_scl_y_prob.extend(dto_scl.predict_proba(X_test_scl)[::,1])
+
+	# DT trained on SMOTE data set
+	if smote_dt_scl:
+		smote = SMOTE()
+		X_smote, y_smote = smote.fit_sample(X_train_scl, y_train)
+		dts_scl = DecisionTreeClassifier(max_depth = treeDepth)
+		dts_scl.fit(X_smote, y_smote)
+		dts_scl_y_pred.extend(dts_scl.predict(X_test_scl).tolist())
+		dts_scl_y_prob.extend(dts_scl.predict_proba(X_test_scl)[::,1])
+
+	# DT trained on upndersampled data set
+	if under_dt_scl:
+		rus = RandomUnderSampler(return_indices=True)
+		X_rus, y_rus, id_rus = rus.fit_sample(X_train_scl, y_train)
+		dtu_scl = DecisionTreeClassifier(max_depth = treeDepth)
+		dtu_scl.fit(X_rus, y_rus)
+		dtu_scl_y_pred.extend(dtu_scl.predict(X_test_scl).tolist())
+		dtu_scl_y_prob.extend(dtu_scl.predict_proba(X_test_scl)[::,1])
 
 	# DT trained on oversampled data set
 	if over_dt:
@@ -94,7 +182,7 @@ for train_index, test_index in kf.split(X, y):
 	if smote_dt:
 		smote = SMOTE()
 		X_smote, y_smote = smote.fit_sample(X_train, y_train)
-		dts = DecisionTreeClassifier()
+		dts = DecisionTreeClassifier(max_depth = treeDepth)
 		dts.fit(X_smote, y_smote)
 		dts_y_pred.extend(dts.predict(X_test).tolist())
 		dts_y_prob.extend(dts.predict_proba(X_test)[::,1])
@@ -103,7 +191,7 @@ for train_index, test_index in kf.split(X, y):
 	if under_dt:
 		rus = RandomUnderSampler(return_indices=True)
 		X_rus, y_rus, id_rus = rus.fit_sample(X_train, y_train)
-		dtu = DecisionTreeClassifier()
+		dtu = DecisionTreeClassifier(max_depth = treeDepth)
 		dtu.fit(X_rus, y_rus)
 		dtu_y_pred.extend(dtu.predict(X_test).tolist())
 		dtu_y_prob.extend(dtu.predict_proba(X_test)[::,1])
@@ -112,104 +200,48 @@ for train_index, test_index in kf.split(X, y):
 	if clst_dt:
 		cc = ClusterCentroids()
 		X_cc, y_cc = cc.fit_sample(X_train, y_train)
-		dtc = DecisionTreeClassifier()
+		dtc = DecisionTreeClassifier(max_depth = treeDepth)
 		dtc.fit(X_cc, y_cc)
 		dtc_y_pred.extend(dtc.predict(X_test).tolist())
 		dtc_y_prob.extend(dtc.predict_proba(X_test)[::,1])
 
 if reg_dt:
-	print("Decision Tree Trained on Unbalanced Data:")
-	cm = confusion_matrix(y_true, dt_y_pred)
-	print("TN = " + str(cm[0][0]))
-	print("FP = " + str(cm[0][1]))
-	print("FN = " + str(cm[1][0]))
-	print("TP = " + str(cm[1][1]))
-	print("Pr = " + str(cm[1][1]/(cm[1][1] + cm[0][1])))
-	print("Re = " + str(cm[1][1]/(cm[1][1] + cm[1][0])))
-	print("Confusion Matrix: \n" + str(cm))
-	print()
+	report(y_true, dt_y_pred, dt_y_prob, "Decision Tree Trained on Unbalanced Data")
 
-print("Decision Tree Trained on SMOTE Data:")
-cm = confusion_matrix(y_true, dts_y_pred)
-print("TN = " + str(cm[0][0]))
-print("FP = " + str(cm[0][1]))
-print("FN = " + str(cm[1][0]))
-print("TP = " + str(cm[1][1]))
-print("Pr = " + str(cm[1][1]/(cm[1][1] + cm[0][1])))
-print("Re = " + str(cm[1][1]/(cm[1][1] + cm[1][0])))
-print("Confusion Matrix: \n" + str(cm))
-print()
+if reg_dt_std:
+	report(y_true, dt_std_y_pred, dt_std_y_prob, "Decision Tree Trained on on Standardized Unbalanced Data")
+
+if reg_dt_scl:
+	report(y_true, dt_scl_y_pred, dt_scl_y_prob, "Decision Tree Trained on Scaled Unbalanced Data")
+
+if smote_dt_scl:
+	report(y_true, dts_scl_y_pred, dts_scl_y_prob, "Decision Tree Trained on Scaled SMOTE Data")
+
+if under_dt_scl:
+	report(y_true, dtu_scl_y_pred, dtu_scl_y_prob, "Decision Tree Trained on Scaled UnderSampled Data")
+
+if over_dt_scl:
+	report(y_true, dto_scl_y_pred, dto_scl_y_prob, "Decision Tree Trained on Scaled Oversampled Data")
+
+if smote_dt:
+	report(y_true, dts_y_pred, dts_y_prob, "Decision Tree Trained on SMOTE Data")
+
+if under_dt:
+	report(y_true, dtu_y_pred, dtu_y_prob, "Decision Tree Trained on UnderSampled Data")
+
+if over_dt:
+	report(y_true, dto_y_pred, dto_y_prob, "Decision Tree Trained on Oversampled Data")
+
+if clst_dt:
+	report(y_true, dtc_y_pred, dtc_y_prob, "Decision Tree Trained on ClusterCentroids Data")
+
+if rand:
+	report(y_true, dc_y_pred, dc_y_prob, "Random Classifier")
+
+
+plt.legend(loc=1)
+plt.title("Precision Recall Curve")
+plt.show()
+
 # print ("Accuracy : " + str(accuracy_score(y_true,dt_y_pred)*100))
 # print("Report : \n\n" + str(classification_report(y_true, dt_y_pred)))
-
-print("Decision Tree Trained on UnderSampled Data:")
-cm = confusion_matrix(y_true, dtu_y_pred)
-print("TN = " + str(cm[0][0]))
-print("FP = " + str(cm[0][1]))
-print("FN = " + str(cm[1][0]))
-print("TP = " + str(cm[1][1]))
-print("Pr = " + str(cm[1][1]/(cm[1][1] + cm[0][1])))
-print("Re = " + str(cm[1][1]/(cm[1][1] + cm[1][0])))
-print("Confusion Matrix: \n" + str(cm))
-print()
-# print ("Accuracy : " + str(accuracy_score(y_true,dtu_y_pred)*100))
-# print("Report : \n" + str(classification_report(y_true, dtu_y_pred)))
-
-print("Decision Tree Trained on Oversampled Data:")
-cm = confusion_matrix(y_true, dto_y_pred)
-print("TN = " + str(cm[0][0]))
-print("FP = " + str(cm[0][1]))
-print("FN = " + str(cm[1][0]))
-print("TP = " + str(cm[1][1]))
-print("Pr = " + str(cm[1][1]/(cm[1][1] + cm[0][1])))
-print("Re = " + str(cm[1][1]/(cm[1][1] + cm[1][0])))
-print("Confusion Matrix: \n" + str(cm))
-print()
-
-print("Decision Tree Trained on ClusterCentroids Data:")
-cm = confusion_matrix(y_true, dtc_y_pred)
-print("TN = " + str(cm[0][0]))
-print("FP = " + str(cm[0][1]))
-print("FN = " + str(cm[1][0]))
-print("TP = " + str(cm[1][1]))
-print("Pr = " + str(cm[1][1]/(cm[1][1] + cm[0][1])))
-print("Re = " + str(cm[1][1]/(cm[1][1] + cm[1][0])))
-print("Confusion Matrix: \n" + str(cm))
-print()
-
-# print ("Accuracy : " + str(accuracy_score(y_true, dto_y_pred)*100))
-# print("Report : \n" + str(classification_report(y_true, dto_y_pred)))
-
-print("Random Classifier:")
-cm = confusion_matrix(y_true, dc_y_pred)
-print("TN = " + str(cm[0][0]))
-print("FP = " + str(cm[0][1]))
-print("FN = " + str(cm[1][0]))
-print("TP = " + str(cm[1][1]))
-print("Pr = " + str(cm[1][1]/(cm[1][1] + cm[0][1])))
-print("Re = " + str(cm[1][1]/(cm[1][1] + cm[1][0])))
-print("Confusion Matrix: \n" + str(cm))
-print()
-# print ("Accuracy : " + str(accuracy_score(y_true,dc_y_pred)*100))
-# print("Report : \n" + str(classification_report(y_true, dc_y_pred)))
-
-
-
-# precision, recall, thresholds = precision_recall_curve( y_true, dtu_y_prob)
-# average_precision = average_precision_score(y_true, dtu_y_prob)
-# plt.plot(recall, precision,label="DecisionTree With Under Sampled Training Data, auc = {0:.4f}".format(average_precision))
-
-# precision, recall, thresholds = precision_recall_curve( y_true, dto_y_prob)
-# average_precision = average_precision_score(y_true, dto_y_prob)
-# plt.plot(recall, precision,label="DecisionTree With Over Sampled Training Data, auc = {0:.4f}".format(average_precision))
-
-# precision, recall, thresholds = precision_recall_curve( y_true, dt_y_prob)
-# average_precision = average_precision_score(y_true, dt_y_prob)
-# plt.plot(recall, precision,label="DecisionTreeClassifier, auc = {0:.4f}".format(average_precision))
-
-# precision, recall, thresholds = precision_recall_curve(y_true, dc_y_prob)
-# average_precision = average_precision_score(y_true, dc_y_prob)
-# plt.plot(recall, precision,label="DummyClassifier, auc={0:.4f}".format(average_precision))
-# plt.legend(loc=1)
-# plt.title("Precision Recall Curve")
-# plt.show()
