a5be90ee64461e40945e93d72d743251fd60e789
Ben Earle <ben.earle@live.ca>
2019-11-30 18:57:26 -0800

    Finished RFE test

--- RFE_feature_extract.py
+++ RFE_feature_extract.py
@@ -11,8 +11,11 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 import preprocessing as prc
 
-def report(y_true, y_pred, y_prob):
+def report(name, y_true, y_pred, y_prob):
 	cm = confusion_matrix(y_true, y_pred)
+	precision, recall, thresholds = precision_recall_curve( y_true, y_prob)
+	average_precision = average_precision_score(y_true, y_prob)
+	PrecisionAtRe50_DT = np.max(precision[recall>=0.5])
 	print("TN = " + str(cm[0][0]))
 	print("FP = " + str(cm[0][1]))
 	print("FN = " + str(cm[1][0]))
@@ -22,28 +25,37 @@ def report(y_true, y_pred, y_prob):
 	print("Confusion Matrix: \n" + str(cm))
 	print()
 
-	precision, recall, thresholds = precision_recall_curve( y_true, y_prob)
-	average_precision = average_precision_score(y_true, y_prob)
-	plt.plot(recall, precision,label=name+", auc = {0:.4f}".format(average_precision))
+	print('Pr@Re50 = ', PrecisionAtRe50_DT)
+	plt.plot(recall, precision,label="Pr@Re>50 = {0:.5f}".format(PrecisionAtRe50_DT))
 
 df = pd.read_csv('Files/csv_result-Descriptors_Training.csv', sep=',')
 df = df.drop(['id'], axis=1).replace(['P', 'N'], [1, 0])
-df = prc.handle_outlier(prc.detect_outlier_iterative_IQR(df).dropna(thresh=20))
+# df = prc.handle_outlier(prc.detect_outlier_iterative_IQR(df).dropna(thresh=20))
 
-dt = DecisionTreeClassifier()
+dt_rfe = DecisionTreeClassifier(max_depth=4)
+dt = DecisionTreeClassifier(max_depth=4)
 
 X = df.drop(['class'], axis=1)
 y = df['class']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1, stratify=y)
 
-rfe = RFE(dt, 7)
+rfe = RFE(dt_rfe, 7)
 
 X_train_rfe = rfe.fit_transform(X_train,y_train)
+print("RFE feature ranking:")
+print(rfe.ranking_)
+print()
 X_test_rfe = rfe.transform(X_test)
 
-dt.fit(X_train_rfe,y_train)
-y_pred = dt.predict(X_test_rfe)
-y_prob = dt.predict_proba(X_test_rfe)
+dt_rfe.fit(X_train_rfe,y_train)
+y_pred = dt_rfe.predict(X_test_rfe)
+y_prob = dt_rfe.predict_proba(X_test_rfe)[::,1]
 
-report(y_test, y_pred, y_prob)
+report("RFE: ", y_test, y_pred, y_prob)
+
+dt.fit(X_train,y_train)
+report("RFE: ", y_test, dt.predict(X_test), dt.predict_proba(X_test)[::,1])
+plt.legend(loc=1)
+plt.title("Precision Recall Curve")
+plt.show()
