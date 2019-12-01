d4c7aacd31ffb98023a0e97ad4f421677f699e75
Ben Earle <ben.earle@live.ca>
2019-11-30 19:03:04 -0800

    Cleaned RFE test

--- RFE_feature_extract.py
+++ RFE_feature_extract.py
@@ -28,34 +28,42 @@ def report(name, y_true, y_pred, y_prob):
 	print('Pr@Re50 = ', PrecisionAtRe50_DT)
 	plt.plot(recall, precision,label="Pr@Re>50 = {0:.5f}".format(PrecisionAtRe50_DT))
 
+def test_model(model, X_train, X_test, y_train):
+	model.fit(X_train, y_train)
+	y_pred = dt_rfe.predict(X_test_rfe)
+	y_prob = dt_rfe.predict_proba(X_test_rfe)[::,1]
+	return y_pred, y_prob
+
+
 df = pd.read_csv('Files/csv_result-Descriptors_Training.csv', sep=',')
 df = df.drop(['id'], axis=1).replace(['P', 'N'], [1, 0])
 # df = prc.handle_outlier(prc.detect_outlier_iterative_IQR(df).dropna(thresh=20))
 
-dt_rfe = DecisionTreeClassifier(max_depth=4)
-dt = DecisionTreeClassifier(max_depth=4)
-
 X = df.drop(['class'], axis=1)
 y = df['class']
 
+# Split data for simple test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1, stratify=y)
 
+# Do RFE
 rfe = RFE(dt_rfe, 7)
-
 X_train_rfe = rfe.fit_transform(X_train,y_train)
+X_test_rfe = rfe.transform(X_test)
 print("RFE feature ranking:")
 print(rfe.ranking_)
 print()
-X_test_rfe = rfe.transform(X_test)
 
-dt_rfe.fit(X_train_rfe,y_train)
-y_pred = dt_rfe.predict(X_test_rfe)
-y_prob = dt_rfe.predict_proba(X_test_rfe)[::,1]
-
+# Train and test classifier with RFE
+dt_rfe = DecisionTreeClassifier(max_depth=4)
+y_pred, y_prob = test_model(dt_rfe, X_train_rfe, y_train, X_test_rfe)
 report("RFE: ", y_test, y_pred, y_prob)
 
-dt.fit(X_train,y_train)
+# Train and test classifier without RFE
+dt = DecisionTreeClassifier(max_depth=4)
+y_pred, y_prob = test_model(dt_rfe, X_train_rfe, y_train, X_test_rfe)
 report("RFE: ", y_test, dt.predict(X_test), dt.predict_proba(X_test)[::,1])
+
+# Display PR Curve
 plt.legend(loc=1)
 plt.title("Precision Recall Curve")
 plt.show()
